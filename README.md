e7b2c391ccd143c8a365bc9534b27b24c421de49
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-10-13 17:15:47 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# rps checker
+
+some random rock paper scissors checker
+
+uhh yeah docs at https://rps.srizan.ml k bye
---
+++ index.ts
@@ -0,0 +1,32 @@
+/**
+ * @async
+ * @description Main function, you need to pass 2 numbers.
+ * @param {number} player1 1 for rock, 2 for paper, 3 for scissors
+ * @param {number} player2 1 for rock, 2 for paper, 3 for scissors
+ * @returns {string} either "player1", "player2" (whoever won) or "tie" for a tie.
+ */
+
+function rockpaperscissors(
+	player1: number,
+	player2: number
+): string {
+        if (player1 === player2) {
+            return "tie"
+		} else if (player1 === 1 && player2 === 2) {
+            return "player2"
+		} else if (player1 === 2 && player2 === 1) {
+            return "player1"
+        } else if (player1 === 1 && player2 === 3) {
+            return "player1"
+        } else if (player1 === 3 && player2 === 1) {
+            return "player2"
+        } else if (player1 === 2 && player2 === 3) {
+            return "player2"
+        } else if (player1 === 3 && player2 === 2) {
+            return "player1"
+        } else {
+            throw new Error("Make sure player1 and player2 have right numbers.")
+        }
+}
+
+export default rockpaperscissors;
