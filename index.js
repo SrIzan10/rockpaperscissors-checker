7603a077310c5bb87b974e481a3ea0e36d8039a7
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-10-28 18:38:57 +0200

    feat: make it esm

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 "use strict";
-/**
- * @example
- * // should return player1 (the winner)
- * rockpaperscissors(2, 1)
- * @example
- * // should return tie (it's a tie)
- * rockpaperscissors(1, 1)
- * @description Main function, you need to pass 2 numbers.
- * @param {number} player1 1 for rock, 2 for paper, 3 for scissors
- * @param {number} player2 1 for rock, 2 for paper, 3 for scissors
- * @returns {string} either "player1", "player2" (whoever won) or "tie" for a tie.
- */
-exports.__esModule = true;
 function rockpaperscissors(player1, player2) {
     if (player1 === player2) {
         return "tie";
@@ -38,4 +25,5 @@ function rockpaperscissors(player1, player2) {
         throw new Error("Make sure player1 and player2 have right numbers.");
     }
 }
-exports["default"] = rockpaperscissors;
+export const __esModule = true;
+export default rockpaperscissors;
